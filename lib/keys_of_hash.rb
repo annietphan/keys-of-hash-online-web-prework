32d607df60b6f7fefab72ec6045d624254151849
Annie Phan <anniephan.atp@gmail.com>
2019-03-06 23:11:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+def animals
+  animals = {
+    "sugar glider" => "Australia",
+    "aye-aye" => "Madagascar",
+    "red-footed tortoise" => "Panama",
+    "kangaroo" => "Australia",
+    "tomato frog" => "Madagascar",
+    "koala" => "Australia"
+  }
 class Hash
   def keys_of(arguments)
     # code goes here
+    array = []
+    self.each do |k, v|
+      arguments.each do |i|
+        if i == v
+          array.push(k)
+        end
+      end
+    end
+    return array
   end
 end
